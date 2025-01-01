aebe207e1d1edabac7e6f47cc1ed7222f2ae0053
alis04ka <alisa.dubkova21@gmail.com>
2025-01-01 16:06:55 +0100

    add lookup ivfflat

--- src/include/storage/vectordb/ivfflat.h
+++ src/include/storage/vectordb/ivfflat.h
@@ -1,38 +1,43 @@
-#include <iostream>
-#include "benchmark/adapters/leanstore_adapter.h"
 #include "schema.h"
 #include "storage/vectordb/ivfflat_adapter.h"
+#include <iostream>
 
 namespace leanstore::storage::vector {
 
 template <typename T>
 void print_vec(T vec) {
-  for (auto elem : vec) { std::cout << elem << ", "; }
+  for (auto elem : vec) {
+    std::cout << elem << ", ";
+  }
   std::cout << "\n";
 }
 
 struct Centroid {
-  std::vector<const BlobState*> bucket;
+  std::vector<const BlobState *> bucket;
 };
 
 float distance_vec(std::span<const float> span1, std::span<const float> span2);
 float distance_blob(VectorAdapter &db, const BlobState *blob1, const BlobState *blob2);
+float distance_vec_blob(VectorAdapter &db, std::span<const float> input_span, const BlobState *blob_state);
+
+int calculate_num_centroids(int num_vec);
+int calculate_num_probe_centroids(int num_centroids);
+
 int find_bucket(VectorAdapter &db, const BlobState *input_vec);
+std::vector<int> find_k_closest_centroids(VectorAdapter &db, const std::vector<float> &input_vec, size_t k);
 void initialize_centroids(VectorAdapter &db, size_t num_centroids);
-void update_one_centroid(VectorAdapter &db, std::vector<const BlobState *> bucket, const VectorRecord::Key &key);
+void update_one_centroid(VectorAdapter &db, std::vector<const BlobState *> bucket, const VectorRecord::Key &key, size_t vector_size);
 
 class IVFFlatNoCopyIndex {
- public:
+public:
   IVFFlatNoCopyIndex(VectorAdapter db, size_t num_centroids, size_t num_probe_centroids, size_t vector_size);
 
-  void load_vectors();
   void assign_vectors_to_centroids();
   void update_centroids();
   void BuildIndex();
-  void InsertVectorEntry(VectorRecord &record);
-  std::vector<std::pair<float, const BlobState*>> lookup_k_nearest(BlobState* query_blob, size_t k);
+  std::vector<const BlobState *> find_n_closest_vectors(const std::vector<float> &input_vec, size_t n);
 
- private:
+private:
   VectorAdapter db;
   size_t num_centroids;
   size_t num_probe_centroids;
@@ -40,7 +45,6 @@ class IVFFlatNoCopyIndex {
   std::vector<Centroid> centroids;
   std::vector<const BlobState *> vectors;
   std::vector<std::array<uint8_t, BlobState::MAX_MALLOC_SIZE>> vectors_storage;
-  
 };
 
-}  // namespace leanstore::storage::vector
+} // namespace leanstore::storage::vector
--- src/include/storage/vectordb/ivfflat_adapter.h
+++ src/include/storage/vectordb/ivfflat_adapter.h
@@ -1,16 +1,15 @@
 #pragma once
 
+#include <typeindex>
 #include "leanstore/kv_interface.h"
 #include "leanstore/leanstore.h"
 #include "schema.h"
-#include <typeindex>
-#include <typeinfo>
 
 namespace leanstore::storage::vector {
 
-using FoundVectorFunc = std::function<bool(const VectorRecord::Key&,  const VectorRecord &)>;
+using FoundVectorFunc  = std::function<bool(const VectorRecord::Key &, const VectorRecord &)>;
 using AccessVectorFunc = std::function<void(const VectorRecord &)>;
-using ModifyVectorFunc = std::function<void(VectorRecord&)>;
+using ModifyVectorFunc = std::function<void(VectorRecord &)>;
 
 static constexpr uint64_t GLOBAL_BLOCK_SIZE = 4096;
 
@@ -32,13 +31,10 @@ struct VectorAdapter {
   ~VectorAdapter() = default;
 
   // -------------------------------------------------------------------------------------
-  void Scan(const VectorRecord::Key &key,
-            const FoundVectorFunc& found_db);
+  void Scan(const VectorRecord::Key &key, const FoundVectorFunc &found_db);
   void ScanDesc(const VectorRecord::Key &key, const FoundVectorFunc &found_record_cb);
-  void ScanCentroids(const VectorRecord::Key &key,
-            const FoundVectorFunc& found_db);
-  void ScanCentroidsDesc(const VectorRecord::Key &key,
-                const FoundVectorFunc &found_record_cb);
+  void ScanCentroids(const VectorRecord::Key &key, const FoundVectorFunc &found_db);
+  void ScanCentroidsDesc(const VectorRecord::Key &key, const FoundVectorFunc &found_record_cb);
   void InsertVectorRecordIntoMain(const VectorRecord::Key &r_key, const VectorRecord &record);
   void InsertVectorRecordIntoCentroids(const VectorRecord::Key &r_key, const VectorRecord &record);
   void UpdateCentroid(const VectorRecord::Key &key, std::span<u8> updated_data);
@@ -47,16 +43,17 @@ struct VectorAdapter {
   auto LookUpCentroids(const VectorRecord::Key &r_key, const AccessVectorFunc &fn) -> bool;
   auto GetFloatVectorFromMain(const VectorRecord::Key &key) -> std::vector<float>;
   auto GetFloatVectorFromCentroids(const VectorRecord::Key &key) -> std::vector<float>;
+  auto GetFloatVectorFromBlobState(const BlobState *blob_state) -> std::vector<float>;
   auto CountMain() -> u64;
   auto CountCentroids() -> u64;
 
   // -------------------------------------------------------------------------------------
   auto RegisterBlob(std::span<const u8> blob_payload) -> const BlobState *;
-  auto UpdateBlob(std::span<const u8> blob_payload, leanstore::BlobState* prev_blob) -> const BlobState*;
-  void LoadBlob(const BlobState* state, const std::function<void(std::span<const u8>)> &read_cb, bool partial_load);
-  void RemoveBlob(leanstore::BlobState * state) ;
+  auto UpdateBlob(std::span<const u8> blob_payload, leanstore::BlobState *prev_blob) -> const BlobState *;
+  void LoadBlob(const BlobState *state, const std::function<void(std::span<const u8>)> &read_cb, bool partial_load);
+  void RemoveBlob(leanstore::BlobState *state);
 };
 
 static_assert(GLOBAL_BLOCK_SIZE == leanstore::BLK_BLOCK_SIZE);
 
-}
+}  // namespace leanstore::storage::vector
